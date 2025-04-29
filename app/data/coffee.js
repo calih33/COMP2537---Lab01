0b659531ff6f8f36f59f6aec05419b800669f212
calih33 <caliheung@gmail.com>
2025-04-28 22:01:03 -0700

    merged all foldrs

---
+++ app/data/coffee.js
@@ -0,0 +1,82 @@
+[
+    {
+        "name": "Nemesis",
+        "rating": "4.7/5",
+        "location": "Gastown",
+        "specialty": "Hand-brewed Coffee",
+        "study_friendly": "No",
+        "price": "$$$"
+    },
+    {
+        "name": "Prototype",
+        "rating": "4.6/5",
+        "location": "Main St.",
+        "specialty": "Espresso/Donuts",
+        "study_friendly": "No",
+        "price": "$$"
+    },
+    {
+        "name": "49th Parallel",
+        "rating": "4.8/5",
+        "location": "Strathcona",
+        "specialty": "In-House Coffee",
+        "study_friendly": "No",
+        "price": "$$"
+    },
+    {
+        "name": "Revolver",
+        "rating": "4/5",
+        "location": "Downtown",
+        "specialty": "Pour-Over",
+        "study_friendly": "No",
+        "price": "$$"
+    },
+    {
+        "name": "Small Victory",
+        "rating": "4.7/5",
+        "location": "Brentwood",
+        "specialty": "Flat whites & pastries",
+        "study_friendly": "Yes",
+        "price": "$$"
+    },
+    {
+        "name": "Timbertrain",
+        "rating": "4.5/5",
+        "location": "East Vancouver",
+        "specialty": "Locally roasted beans",
+        "study_friendly": "Yes",
+        "price": "$$"
+    },
+    {
+        "name": "Kafka's",
+        "rating": "4.6/5",
+        "location": "Mount Pleasant",
+        "specialty": "Third-wave espresso",
+        "study_friendly": "Yes",
+        "price": "$"
+    },
+    {
+        "name": "Pallet",
+        "rating": "4.3/5",
+        "location": "Broadway",
+        "specialty": "Single-origin coffee",
+        "study_friendly": "Yes",
+        "price": "$"
+    },
+    {
+        "name": "Aperture",
+        "rating": "4.2/5",
+        "location": "Cambie St.",
+        "specialty": "Italian-style espresso",
+        "study_friendly": "High",
+        "price": "$$"
+    },
+    {
+        "name": "Elysian",
+        "rating": "4.5/5",
+        "location": "Downtown",
+        "specialty": "Slow-bar coffee",
+        "study_friendly": "Yes",
+        "price": "$$"
+    }
+]
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Cali Heung, A01018207, Set B, 02/18/2025
+
+This assignment is 100% complete.
+
